b3b9f8b78de4c2a74eac68ee4b793f5bb4733b10
SensitiveLion <andypassidomo@gmail.com>
2015-06-18 14:26:48 -0400

    creating database

--- db/migrate/20150618005059_create_job_listing.rb
+++ db/migrate/20150618005059_create_job_listing.rb
@@ -1,6 +1,11 @@
 class CreateJobListing < ActiveRecord::Migration
   def change
     create_table :job_listings do |t|
+      t.integer :user_id, null: false
+      t.integer :company_id, null: false
+      t.string :title, null: false
+      t.string :description, null: false
+      t.string :contact
     end
   end
 end
--- db/migrate/20150618005859_create_company.rb
+++
@@ -1,6 +0,0 @@
-class CreateCompany < ActiveRecord::Migration
-  def change
-    create_table :companies do |t|
-    end
-  end
-end
